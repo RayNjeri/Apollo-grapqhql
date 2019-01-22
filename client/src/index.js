690c8533e628906fa85579dce6762b872abf1b3c
Rachael Njeri <rachael.njeri@andela.com>
2019-01-22 16:06:56 +0300

    Fetching a list with pagination
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+
+import Pages from './pages';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000/'
 });
+
 const client = new ApolloClient({
   cache,
   link
@@ -26,3 +32,10 @@ client
     `
   })
   .then(result => console.log(result));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
